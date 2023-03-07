0148f1fc192b6078956e108c3177fa225fda2ea1
AngrySusanoo <126177355+AngrySusanoo@users.noreply.github.com>
2023-03-07 08:05:03 -0500

    Update Rating.js

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -57,6 +57,17 @@ function Rating(props) {
           }
         />
       </span>
+      <span>
+        <i
+          className={
+            rating >= 3
+              ? 'fas fa-star'
+              : rating = 5
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        />
+      </span>
       {caption ? (
         <span>{caption}</span>
       ) : (
