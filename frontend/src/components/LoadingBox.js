a545c5297fb34bea3b0cd7e8ad71c53a48d31c8b
AngrySusanoo <126177355+AngrySusanoo@users.noreply.github.com>
2023-03-06 19:46:18 -0500

    Update LoadingBox.js

--- frontend/src/components/LoadingBox.js
+++ frontend/src/components/LoadingBox.js
@@ -4,6 +4,7 @@ export default function LoadingBox() {
   return (
     <Spinner animation="border" role="status">
       <span className="visually-hidden">Loading...</span>
+      <span className="visually-visible">Hello World</span>
     </Spinner>
   );
 }
